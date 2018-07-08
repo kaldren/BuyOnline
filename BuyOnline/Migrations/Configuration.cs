c356bc367d1fdae86a71a9866e4b0b5e2e71d3b9
drenski666 <drenski666@hotmail.com>
2018-07-08 08:40:58 +0300

    Init Identity Migrations

---
+++ BuyOnline/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BuyOnline.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BuyOnline.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BuyOnline.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
